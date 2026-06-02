3f9700ca1117fcc9a6401bb8c32560724293d1bf
nord-sayt <1292mohammadamin1292@gmail.com>
2026-06-02 16:58:40 +0330

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,21 @@
+// 🔥 اتصال اصلی به Firebase
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+// 👇 اینجا کانفیگ Firebase خودتو بذار
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_BUCKET",
+  messagingSenderId: "YOUR_ID",
+  appId: "YOUR_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
